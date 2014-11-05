e610fe343e6c18f5e82bfac4c72b0f0e2ec549ec
Margot Darby <margotdarby@gmail.com>
2014-11-05 07:57:28 -0500

    new spb code additions

--- Ch 02/sp-blogger/app/js/app.js
+++ Ch 02/sp-blogger/app/js/app.js
@@ -3,3 +3,5 @@
 angular.module('spBlogger',['spBlogger.posts','spBlogger.controllers','spBlogger.directives','spBlogger.filters','spBlogger.services']);
 
 angular.module('spBlogger').value('version','V1.0');
+
+angular.module('spBlogger').run(['state , function(state){ $state.go('allPosts'); }]);
